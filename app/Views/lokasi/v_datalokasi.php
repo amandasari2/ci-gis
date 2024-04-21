77ec74a3cc2ce0bfe95735dbc59c04e76810abfe
amandasari2 <amandasari968@gmail.com>
2024-04-21 16:41:43 +0700

    Update CI GIS 4

--- app/Views/lokasi/v_datalokasi.php
+++ app/Views/lokasi/v_datalokasi.php
@@ -10,9 +10,9 @@
         <table class="table table-bordered" id="datatablesSimple">
             <thead>
                 <tr>
-                    <th width="30px">No.</th>
-                    <th>Nama Lokasi</th>
-                    <th>Alamat Lokasi</th>
+                    <th width="20px">No.</th>
+                    <th width="80px">Nama Lokasi</th>
+                    <th width="70px">Alamat Lokasi</th>
                     <th>Coordinat</th>
                     <th>Foto</th>
                     <th width="150px">Aksi</th>
@@ -26,7 +26,7 @@
                         <td><?= $value['nama_lokasi'] ?></td>
                         <td><?= $value['alamat_lokasi'] ?></td>
                         <td><?= $value['latitude'] ?>, <?= $value['longitude'] ?></td>
-                        <td><img src="<?= base_url('foto/' . $value['foto_lokasi']) ?>" width="150px"></td>
+                        <td><img src="<?= base_url('foto/' . $value['foto_lokasi']) ?>" width="100px"></td>
                         <td>
                             <a href="<?= base_url('Lokasi/editLokasi/' . $value['id_lokasi']) ?>" class="btn btn-warning">Edit</a>
                             <a href="<?= base_url('Lokasi/deleteLokasi/' . $value['id_lokasi']) ?>" class="btn btn-danger" onclick="return confirm('Yakin Menghapus Data?')">Delete</a>
--- app/Views/v_dashboard.php
+++ app/Views/v_dashboard.php
@@ -1,1 +1,14 @@
-<h1>Ini Page Dashboard</h1>
+<div class="col-xl-12 col-md-12">
+    <div class="card bg-primary text-white mb-4">
+    <div class="inner">
+             <h3><?= $jumlahlokasi ?></h3>
+
+             <p>Data Lokasi</p>
+         </div>
+        
+        <div class="card-footer d-flex align-items-center justify-content-between">
+            <a class="small text-white stretched-link" href="<?= base_url('Lokasi') ?>">View Details</a>
+            <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+        </div>
+    </div>
+</div>
